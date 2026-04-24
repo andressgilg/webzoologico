8cec8926af19d1ecc24efef388ed504412e1bfeb
andressgilg <andress.gilg@konradlorenz.edu.co>
2026-04-23 19:43:30 -0500

    Clase 23/04/26
    Se realizó la diapositiva ANGULAR- API CRUD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { AnimalComponent } from './componets/animal-component/animal-component'
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'inicio', component: AnimalComponent }
+];
