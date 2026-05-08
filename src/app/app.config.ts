a5f846c825fbd591b4b57fd5b507b7145fb46e84
dasdas <asdasd@gmail.com>
2026-05-07 20:33:40 -0500

    Clase 07/05/2026
    SEMANA 12

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideToastr, ToastrModule } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule, ToastrModule.forRoot()),
+    provideToastr(),
+    provideAnimations(),
   ]
 };
