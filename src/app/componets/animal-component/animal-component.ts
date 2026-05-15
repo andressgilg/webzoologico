2e0f1509ecbd34c23fb1f312b274dc0963cb66a1
fER <josef.ubaquee@konradlorenz.edu.co>
2026-05-14 19:25:45 -0500

    CLASE SEMANA 13
    14/05/2026 PUT Y DELETE

--- src/app/componets/animal-component/animal-component.ts
+++ src/app/componets/animal-component/animal-component.ts
@@ -16,6 +16,8 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 export class AnimalComponent {
   animalList: any = [];
   animalForm: FormGroup | any;
+  idAnimal: any;
+  editableAnimal: boolean = false;
 
   constructor(private animalService: AnimalService,
     private cd: ChangeDetectorRef,
@@ -60,4 +62,44 @@ export class AnimalComponent {
       }
     );
   }
+  updateAnimalEntry() {
+    //Removiendo valores vacios del formulario de actualización
+    for (let key in this.animalForm.value) {
+      if (this.animalForm.value[key] === '') {
+        this.animalForm.removeControl(key);
+      }
+    }
+    this.animalService.updateAnimal(this.idAnimal, this.animalForm.value).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal editado");
+      }
+    );
+  }
+  toggleEditAnimal(id: any) {
+    this.idAnimal = id;
+    console.log(this.idAnimal)
+    this.animalService.getOneAnimal(id).subscribe(
+      data => {
+        this.animalForm.setValue({
+          nombre: data.nombre,
+          edad: data.edad,
+          tipo: data.tipo,
+        });
+      }
+    );
+    this.editableAnimal = !this.editableAnimal;
+  }
+  deleteAnimalEntry(id: any) {
+    console.log(id)
+    this.animalService.deleteAnimal(id).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal eliminado");
+      }
+    );
+  }
+
+
+
 }
--- src/app/services/animal-service.ts
+++ src/app/services/animal-service.ts
@@ -8,6 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AnimalService {
 
+
   constructor(private http: HttpClient) { }
   apiUri = '/api/animals';
   httpOptions = new HttpHeaders().set('Content-Type', 'application/json');
@@ -21,5 +22,23 @@ export class AnimalService {
       data,
       { headers: this.httpOptions });
   }
+  updateAnimal(id: any, data: any): Observable<any> {
+    console.log(data)
+    return this.http.put<any>(
+      this.apiUri + '/' + id,
+      data,
+      { headers: this.httpOptions });
+  }
+  getOneAnimal(id: any): Observable<any> {
+    return this.http.get<any>(
+      this.apiUri + '/' + id,
+      { headers: this.httpOptions });
+  }
+  deleteAnimal(id: any) {
+    return this.http.delete<any>(
+      this.apiUri + "/" + id,
+      { headers: this.httpOptions });
+  }
+
 
 }
